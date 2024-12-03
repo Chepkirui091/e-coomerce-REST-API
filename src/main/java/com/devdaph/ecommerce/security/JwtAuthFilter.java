5d577c1e609660c44a55b7519fa86fb732959543
daphne <chepkiruidaphne91@gmail.com>
2024-12-03 12:42:03 +0300

    cors configuration and securityconfigs

--- src/main/java/com/devdaph/ecommerce/security/JwtAuthFilter.java
+++ src/main/java/com/devdaph/ecommerce/security/JwtAuthFilter.java
@@ -7,6 +7,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -25,6 +29,23 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String token = getTokenFromRequest(request);
+
+        if (token != null) {
+            String username = jwtUtils.getUSerNameFromToken(token);
+
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+            if (StringUtils.hasText(username) && jwtUtils.isTokenValid(token, userDetails)) {
+                log.info("VALID JWT FOR {}", username);
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken); //set user info in that context instance
+            }
+        }
+        filterChain.doFilter(request, response);
 
     }
 
